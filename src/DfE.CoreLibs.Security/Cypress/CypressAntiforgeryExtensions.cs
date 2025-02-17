3490cec7194bb91442f900a359b79019fbcd883e
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-02-17 17:50:17 +0000

    General refactoring

--- src/DfE.CoreLibs.Security/Cypress/CypressAntiforgeryExtensions.cs
+++ src/DfE.CoreLibs.Security/Cypress/CypressAntiforgeryExtensions.cs
@@ -5,33 +5,33 @@ using Microsoft.Extensions.DependencyInjection;
 namespace DfE.CoreLibs.Security.Cypress
 {
     /// <summary>
-    /// Provides extension methods for registering Cypress-related antiforgery handling in the MVC pipeline.
+    /// Provides extension methods for registering Cypress-related AntiForgery handling in the MVC pipeline.
     /// </summary>
-    public static class CypressAntiforgeryExtensions
+    public static class CypressAntiForgeryExtensions
     {
         /// <summary>
-        /// Registers the <see cref="ICypressRequestChecker"/> and <see cref="CypressAwareAntiforgeryFilter"/> services,
-        /// and inserts the Cypress antiforgery filter globally into the MVC pipeline.
+        /// Registers the <see cref="ICypressRequestChecker"/> and <see cref="CypressAwareAntiForgeryFilter"/> services,
+        /// and inserts the Cypress AntiForgery filter globally into the MVC pipeline.
         /// </summary>
         /// <remarks>
         /// <para>
         /// By calling this method, any incoming request recognized as a Cypress request 
-        /// (per <see cref="ICypressRequestChecker"/>) will skip antiforgery validation.
-        /// Other requests will require antiforgery validation as normal.
+        /// (per <see cref="ICypressRequestChecker"/>) will skip AntiForgery validation.
+        /// Other requests will require AntiForgery validation as normal.
         /// </para>
         /// </remarks>
         /// <returns>
         /// The same <see cref="IMvcBuilder"/> so further MVC configuration can be chained.
         /// </returns>
-        public static IMvcBuilder AddCypressAntiforgeryHandling(this IMvcBuilder mvcBuilder)
+        public static IMvcBuilder AddCypressAntiForgeryHandling(this IMvcBuilder mvcBuilder)
         {
             mvcBuilder.Services.AddScoped<ICypressRequestChecker, CypressRequestChecker>();
 
-            mvcBuilder.Services.AddScoped<CypressAwareAntiforgeryFilter>();
+            mvcBuilder.Services.AddScoped<CypressAwareAntiForgeryFilter>();
 
             mvcBuilder.Services.PostConfigure<MvcOptions>(options =>
             {
-                options.Filters.AddService<CypressAwareAntiforgeryFilter>();
+                options.Filters.AddService<CypressAwareAntiForgeryFilter>();
             });
 
             return mvcBuilder;
--- src/DfE.CoreLibs.Security/Cypress/CypressAwareAntiforgeryFilter.cs
+++ src/DfE.CoreLibs.Security/Cypress/CypressAwareAntiforgeryFilter.cs
@@ -7,12 +7,12 @@ using Microsoft.Extensions.Logging;
 namespace DfE.CoreLibs.Security.Cypress
 {
     /// <summary>
-    /// An authorization filter that enforces antiforgery validation for all requests,
+    /// An authorization filter that enforces AntiForgery validation for all requests,
     /// except for those recognized as valid Cypress requests.
     /// </summary>
-    public class CypressAwareAntiforgeryFilter(
-        IAntiforgery antiforgery,
-        ILogger<CypressAwareAntiforgeryFilter> logger,
+    public class CypressAwareAntiForgeryFilter(
+        IAntiforgery antiForgery,
+        ILogger<CypressAwareAntiForgeryFilter> logger,
         ICypressRequestChecker cypressChecker)
         : IAsyncAuthorizationFilter
     {
@@ -28,12 +28,12 @@ namespace DfE.CoreLibs.Security.Cypress
             var isCypress = cypressChecker.IsCypressRequest(context.HttpContext);
             if (isCypress)
             {
-                logger.LogInformation("Skipping antiforgery for Cypress request");
+                logger.LogInformation("Skipping AntiForgery for Cypress request");
                 return;
             }
 
-            logger.LogInformation("Enforcing antiforgery for non-Cypress request");
-            await antiforgery.ValidateRequestAsync(context.HttpContext);
+            logger.LogInformation("Enforcing AntiForgery for non-Cypress request");
+            await antiForgery.ValidateRequestAsync(context.HttpContext);
         }
     }
 
