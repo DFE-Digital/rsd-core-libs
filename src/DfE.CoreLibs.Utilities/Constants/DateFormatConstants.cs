95ec704f3cc3807e7d3dc27ce4713d48aa550e65
Elijah Aremu <mrelijaha@gmail.com>
2025-04-28 14:10:55 +0100

    DateFormatConstants

---
+++ src/DfE.CoreLibs.Utilities/Constants/DateFormatConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace DfE.CoreLibs.Utilities.Constants;
+
+public static class DateFormatConstants
+{
+    public const string DateWithDayOfTheWeek = "dddd d MMMM yyyy";
+    public const string DateWithoutDayOfTheWeek = "d MMMM yyyy";
+    public const string MonthAndYearFormat = "MMM yyyy";
+    public const string DateUkFormat = "dd/MM/yyyy";
+    public const string YearMonthDayDashed = "yyyy-MM-dd";
+    public const string FullMonthAndYearFormat = "MMMM yyyy";
+}
