40f4d6f1ff231e72d9c77fee937dca0a51312c11
SHAKIR, Muhammad <Muhammad.SHAKIR@EDUCATION.GOV.UK>
2025-06-02 13:37:59 +0100

    Created combined skipping anti forgery process for cypress and specific  requests

---
+++ src/Tests/DfE.CoreLibs.Security.Tests/Antiforgery/CustomAntiForgeryExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using DfE.CoreLibs.Security.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using DfE.CoreLibs.Security.Antiforgery;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using DfE.CoreLibs.Security.Cypress;
+
+namespace DfE.CoreLibs.Security.Tests.Antiforgery
+{
+    public class CustomAntiForgeryExtensionsTests
+    {
+        [Fact]
+        public void AddCustomAntiForgeryHandling_RegistersRequiredServices()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            var mvcBuilder = Substitute.For<IMvcBuilder>();
+            mvcBuilder.Services.Returns(services);
+
+            // Act
+            var result = CustomAntiForgeryExtensions.AddCustomAntiForgeryHandling(mvcBuilder);
+
+            // Assert
+            Assert.Contains(services, d => d.ServiceType == typeof(ICustomRequestChecker) && d.ImplementationType == typeof(CustomRequestChecker));
+
+            Assert.Contains(services, d => d.ServiceType == typeof(ICypressRequestChecker) && d.ImplementationType == typeof(CypressRequestChecker));
+
+            Assert.Contains(services, d => d.ServiceType == typeof(CustomAwareAntiForgeryFilter));
+
+            Assert.Same(mvcBuilder, result);
+
+            var serviceProvider = services.BuildServiceProvider();
+            var mvcOptions = serviceProvider.GetRequiredService<IOptions<MvcOptions>>().Value;
+
+            Assert.Contains(mvcOptions.Filters, filter => filter is ServiceFilterAttribute serviceFilter && serviceFilter.ServiceType == typeof(CustomAwareAntiForgeryFilter));
+        }
+    }
+}
---
+++ src/Tests/DfE.CoreLibs.Security.Tests/Antiforgery/CustomRequestCheckerTests.cs
@@ -0,0 +1,64 @@
+﻿using AutoFixture.AutoNSubstitute;
+using AutoFixture;
+using Microsoft.AspNetCore.Http; 
+using DfE.CoreLibs.Security.Antiforgery;
+
+namespace DfE.CoreLibs.Security.Tests.Antiforgery
+{
+    public class CustomRequestCheckerTests
+    {
+        private readonly IFixture _fixture = new Fixture().Customize(new AutoNSubstituteCustomization());
+        private static readonly string HeaderKey = "x-custom-request";
+
+        private static DefaultHttpContext CreateHttpContext(string headerValue)
+        {
+            var httpContext = new DefaultHttpContext(); 
+            httpContext.Request.Headers[HeaderKey] = headerValue;
+            return httpContext;
+        }
+
+        [Fact]
+        public void IsCustomRequest_ReturnsFalse_WhenNoHeaderKey()
+        {
+            // Arrange 
+
+            var httpContext = CreateHttpContext("ruby");
+            var checker = new CustomRequestChecker();
+
+            // Act
+            var result = checker.IsValidRequest(httpContext, null);
+
+            // Assert
+            Assert.False(result);
+        }
+        [Fact]
+        public void IsCustomRequest_ReturnsFalse_WhenHeaderKeyDoesNotMatched()
+        {
+            // Arrange 
+
+            var httpContext = CreateHttpContext("ruby");
+            var checker = new CustomRequestChecker();
+
+            // Act
+            var result = checker.IsValidRequest(httpContext, "x-header-key");
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsCustomRequest_ReturnsTrue_WhenHeaderKeyMatches()
+        {
+            // Arrange 
+
+            var httpContext = CreateHttpContext("ruby");
+            var checker = new CustomRequestChecker();
+
+            // Act
+            var result = checker.IsValidRequest(httpContext, HeaderKey);
+
+            // Assert
+            Assert.True(result);
+        } 
+    }
+}
