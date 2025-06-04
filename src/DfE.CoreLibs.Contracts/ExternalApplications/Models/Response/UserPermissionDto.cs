669500b2296db52dbf518ceae1c7dfb20d9c08e9
Farshad Dashti <78855469+FrostyApeOne@users.noreply.github.com>
2025-06-04 11:54:05 +0100

    Added shared contracts for External Applications (#56)
    Co-authored-by: FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>

---
+++ src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Response/UserPermissionDto.cs
@@ -0,0 +1,11 @@
+﻿using DfE.CoreLibs.Contracts.ExternalApplications.Enums;
+
+namespace DfE.CoreLibs.Contracts.ExternalApplications.Models.Response
+{
+    public class UserPermissionDto
+    {
+        public Guid ApplicationId { get; set; }
+        public string ResourceKey { get; set; } = string.Empty;
+        public AccessType AccessType { get; set; }
+    }
+}
