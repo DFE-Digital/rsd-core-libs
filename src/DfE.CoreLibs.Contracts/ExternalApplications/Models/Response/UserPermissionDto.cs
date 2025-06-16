70e46d67df023ba79dbd1c438e44f29aeabd9ae6
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-06-16 12:14:35 +0100

    Updated the UserPermissionDto to include nullable ApplicationId

--- src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Response/UserPermissionDto.cs
+++ src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Response/UserPermissionDto.cs
@@ -4,7 +4,7 @@ namespace DfE.CoreLibs.Contracts.ExternalApplications.Models.Response;
 
 public class UserPermissionDto
 {
-    public required Guid ApplicationId { get; set; }
+    public Guid? ApplicationId { get; set; } = null;
     public string ResourceKey { get; set; } = string.Empty;
     public required ResourceType ResourceType { get; set; }
     public required AccessType AccessType { get; set; }
