f39f52358c0fc093ab90f6c30e4301b33340e5da
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-06-24 10:56:48 +0100

    Added ApplicationDto to the ExternalApplications

---
+++ src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Response/ApplicationDto.cs
@@ -0,0 +1,15 @@
+﻿namespace DfE.CoreLibs.Contracts.ExternalApplications.Models.Response
+{
+    public class ApplicationDto
+    {
+        public Guid ApplicationId { get; set; }
+        public string ApplicationReference { get; set; } = string.Empty;
+        public Guid TemplateVersionId { get; set; }
+        public string TemplateName { get; set; } = string.Empty;
+        public int? Status { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateSubmitted { get; set; }
+
+
+    }
+}
