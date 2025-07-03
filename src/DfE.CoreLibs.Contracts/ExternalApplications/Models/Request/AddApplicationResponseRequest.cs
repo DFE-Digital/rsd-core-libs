ae4da1d7b9fe50e7ae79f303183603b34e2f005c
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-07-03 13:18:47 +0100

    Added request and dto classses to the ExternalApplications

---
+++ src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Request/AddApplicationResponseRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace DfE.CoreLibs.Contracts.ExternalApplications.Models.Request
+{
+    public sealed record AddApplicationResponseRequest(string ResponseBody);
+}
---
+++ src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Response/ApplicationResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DfE.CoreLibs.Contracts.ExternalApplications.Models.Response
+{
+    public sealed record ApplicationResponseDto(
+        Guid ResponseId,
+        string ApplicationReference,
+        Guid ApplicationId,
+        string ResponseBody,
+        DateTime CreatedOn,
+        Guid CreatedBy);
+}
