bb105aef91a3f82b928ef395885a3fea44cb8996
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-07-10 18:29:19 +0100

    Added UserAuthorizationDto to teh EA Models

---
+++ src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Response/UserAuthorizationDto.cs
@@ -0,0 +1,7 @@
+﻿namespace DfE.CoreLibs.Contracts.ExternalApplications.Models.Response;
+
+public class UserAuthorizationDto
+{
+    public required IEnumerable<UserPermissionDto> Permissions { get; set; }
+    public required IEnumerable<string> Roles { get; set; }
+}
