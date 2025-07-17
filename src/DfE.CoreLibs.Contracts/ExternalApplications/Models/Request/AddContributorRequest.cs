f877efa468940163052288ca7bfdfa35c93f6619
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-07-17 11:22:08 +0100

    Added UserDto and AddContributorRequest models to the External Applications contracts

---
+++ src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Request/AddContributorRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace DfE.CoreLibs.Contracts.ExternalApplications.Models.Request
+{
+    public class AddContributorRequest
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
---
+++ src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Response/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DfE.CoreLibs.Contracts.ExternalApplications.Models.Response
+{
+    public class UserDto
+    {
+        public Guid UserId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public Guid RoleId { get; set; }
+        public UserAuthorizationDto? Authorization { get; set; }
+    }
+}
