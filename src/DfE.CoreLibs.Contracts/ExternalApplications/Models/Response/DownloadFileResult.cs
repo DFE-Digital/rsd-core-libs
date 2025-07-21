a4e2f182a90c643283d73c0bd2d5611bd3c5dd34
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-07-21 15:33:19 +0100

    Added DownloadFileResult to the EA Models

---
+++ src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Response/DownloadFileResult.cs
@@ -0,0 +1,9 @@
+﻿namespace DfE.CoreLibs.Contracts.ExternalApplications.Models.Response
+{
+    public class DownloadFileResult
+    {
+        public Stream FileStream { get; set; } = null!;
+        public string FileName { get; set; } = null!;
+        public string ContentType { get; set; } = null!;
+    }
+}
