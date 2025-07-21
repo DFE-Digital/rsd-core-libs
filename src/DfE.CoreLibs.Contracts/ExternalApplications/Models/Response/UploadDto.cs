13e17de88724ae31c1ece3d760ab2f64a94404ef
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-07-21 15:30:00 +0100

    Added UploadDto to EA models

---
+++ src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Response/UploadDto.cs
@@ -0,0 +1,16 @@
+﻿namespace DfE.CoreLibs.Contracts.ExternalApplications.Models.Response
+{
+
+    public class UploadDto
+    {
+        public Guid Id { get; set; }
+        public Guid ApplicationId { get; set; }
+        public Guid UploadedBy { get; set; }
+        public UserDto? UploadedByUser { get; set; } = null;
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public string OriginalFileName { get; set; } = null!;
+        public string FileName { get; set; } = null!;
+        public DateTime UploadedOn { get; set; }
+    }
+}
