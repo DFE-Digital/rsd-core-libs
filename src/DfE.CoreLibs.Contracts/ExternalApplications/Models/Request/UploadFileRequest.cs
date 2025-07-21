2b01f41d7cbb78f579303a67c3011f86223d61b3
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-07-21 20:27:55 +0100

    Added UploadFileRequest to the EA Models

---
+++ src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Request/UploadFileRequest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DfE.CoreLibs.Contracts.ExternalApplications.Models.Request
+{
+    public class UploadFileRequest
+    {
+        [FromForm(Name = "name")] 
+        public string Name { get; set; } = default!;
+
+        [FromForm(Name = "description")] 
+        public string? Description { get; set; }
+
+        [FromForm(Name = "file")] 
+        public IFormFile File { get; set; } = default!;
+    }
+}
