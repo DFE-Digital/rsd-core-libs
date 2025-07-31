1300daa5773fabb79a2ac7624ba19eb3b064fe67
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-07-31 16:54:18 +0100

    Updated the ExchangeTokenDto to follow industry standard token endpoint response model

--- src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Response/ExchangeTokenDto.cs
+++ src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Response/ExchangeTokenDto.cs
@@ -1,4 +1,28 @@
-﻿namespace DfE.CoreLibs.Contracts.ExternalApplications.Models.Response
+﻿using System.Text.Json.Serialization;
+
+namespace DfE.CoreLibs.Contracts.ExternalApplications.Models.Response
 {
-    public record ExchangeTokenDto(string AccessToken);
+    public class ExchangeTokenDto
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; } = null!;
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; } = "Bearer";
+
+        [JsonPropertyName("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonPropertyName("refresh_token")]
+        public string? RefreshToken { get; set; }
+
+        [JsonPropertyName("scope")]
+        public string? Scope { get; set; }
+
+        [JsonPropertyName("id_token")]
+        public string? IdToken { get; set; }
+
+        [JsonPropertyName("refresh_expires_in")]
+        public int? RefreshExpiresIn { get; set; }
+    }
 }
