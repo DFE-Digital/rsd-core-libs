2c9bac0fd32722b3507764370eb108c89c222d08
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-07-31 21:17:38 +0100

    Added json propperty name to the EA models

--- src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Response/TemplateSchemaDto.cs
+++ src/DfE.CoreLibs.Contracts/ExternalApplications/Models/Response/TemplateSchemaDto.cs
@@ -1,9 +1,18 @@
-﻿namespace DfE.CoreLibs.Contracts.ExternalApplications.Models.Response;
+﻿using System.Text.Json.Serialization;
+
+namespace DfE.CoreLibs.Contracts.ExternalApplications.Models.Response;
 
 public class TemplateSchemaDto
 {
+    [JsonPropertyName("templateId")]
     public required Guid TemplateId { get; set; }
+
+    [JsonPropertyName("templateVersionId")]
     public required Guid TemplateVersionId { get; set; }
+
+    [JsonPropertyName("versionNumber")]
     public required string VersionNumber { get; set; }
+
+    [JsonPropertyName("jsonSchema")]
     public required string JsonSchema { get; set; }
 }
