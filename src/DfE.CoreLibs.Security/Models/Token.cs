f0e9c25753e1cd0e56fe93980509bc95381faf5a
Farshad Dashti <78855469+FrostyApeOne@users.noreply.github.com>
2025-08-04 09:36:11 +0100

    Enhanced and added an additional method to the user token service which returns a richer model (#67)
    Co-authored-by: FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>

---
+++ src/DfE.CoreLibs.Security/Models/Token.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DfE.CoreLibs.Security.Models
+{
+    public class Token
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; } = null!;
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; } = "Bearer";
+
+        [JsonPropertyName("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonPropertyName("refresh_token")]
+        public string? RefreshToken { get; set; }
+
+        [JsonPropertyName("scope")]
+        public string? Scope { get; set; }
+
+        [JsonPropertyName("id_token")]
+        public string? IdToken { get; set; }
+
+        [JsonPropertyName("refresh_expires_in")]
+        public int? RefreshExpiresIn { get; set; }
+    }
+}
