a188af55d5a9727f4fab62b97b768fb1f102321d
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-10-25 22:02:24 +0100

    Added reference and fileId to the FileUploadedEvent

--- src/GovUK.Dfe.CoreLibs.Messaging.Contracts/Messages/Events/FileUploadedEvent.cs
+++ src/GovUK.Dfe.CoreLibs.Messaging.Contracts/Messages/Events/FileUploadedEvent.cs
@@ -1,6 +1,8 @@
 ﻿namespace GovUK.Dfe.CoreLibs.Messaging.Contracts.Messages.Events;
 public record FileUploadedEvent(
+    string? FileId,
     string FileName,
+    string? Reference,
     string? Path,
     bool? IsAzureFileShare,
     string FileUri,
