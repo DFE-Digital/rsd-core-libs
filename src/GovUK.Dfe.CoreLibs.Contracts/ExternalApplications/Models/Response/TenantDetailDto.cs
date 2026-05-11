8c41e6c6018ca5621fb4c641e557c6a84fc8fc6d
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2026-05-11 14:31:00 +0100

    Added Forms Engine tenant related models to the Contracts package

---
+++ src/GovUK.Dfe.CoreLibs.Contracts/ExternalApplications/Models/Response/TenantDetailDto.cs
@@ -0,0 +1,4 @@
+﻿namespace GovUK.Dfe.CoreLibs.Contracts.ExternalApplications.Models.Response
+{
+    public sealed record TenantDetailDto(Guid Id, string Name, string[] FrontendOrigins);
+}
---
+++ src/GovUK.Dfe.CoreLibs.Contracts/ExternalApplications/Models/Response/TenantSummaryDto.cs
@@ -0,0 +1,6 @@
+﻿namespace GovUK.Dfe.CoreLibs.Contracts.ExternalApplications.Models.Response
+{
+    public sealed record TenantSummaryDto(Guid Id, string Name);
+
+
+}
