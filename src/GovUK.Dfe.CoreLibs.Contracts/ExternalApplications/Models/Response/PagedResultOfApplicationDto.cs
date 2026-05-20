81fa4cf4091777019bfceffdff48c34974c195bd
Elijah Aremu <45821029+elielijah321@users.noreply.github.com>
2026-05-20 15:49:43 +0100

    PagedResult (#111)
    * PagedResult
    
    * PagedResultTests
    
    * Run Corelib Contracts tests
    
    * Exclude from code coverage
    
    * remove redundant test contracts
    
    ---------
    
    Co-authored-by: AREMU, Elijah <Elijah.AREMU@EDUCATION.GOV.UK>

---
+++ src/GovUK.Dfe.CoreLibs.Contracts/ExternalApplications/Models/Response/PagedResultOfApplicationDto.cs
@@ -0,0 +1,5 @@
+using System.Text.Json.Serialization;
+
+namespace GovUK.Dfe.CoreLibs.Contracts.ExternalApplications.Models.Response;
+
+public partial class PagedResultOfApplicationDto : PagedResult<ApplicationDto>{}
