ab782cdd0fa1a73948e7bfa4e89d650d00791cc2
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2026-06-29 15:59:43 +0100

    Updated prop names

--- src/GovUK.Dfe.CoreLibs.Contracts/ExternalApplications/Models/Response/CustomApplicationStatusDto.cs
+++ src/GovUK.Dfe.CoreLibs.Contracts/ExternalApplications/Models/Response/CustomApplicationStatusDto.cs
@@ -1,20 +1,23 @@
 using GovUK.Dfe.CoreLibs.Contracts.ExternalApplications.Enums;
 using System;
+using System.Text.Json.Serialization;
 
 namespace GovUK.Dfe.CoreLibs.Contracts.ExternalApplications.Models.Response
 {
-    public sealed class CustomApplicationStatusDto    {
+    public sealed class CustomApplicationStatusDto
+    {
 
+        [JsonPropertyName("customApplicationStatusId")]
         public Guid? CustomApplicationStatusId { get; set; }
-
+        [JsonPropertyName("templateId")]
         public Guid TemplateId { get; set; }
-
+        [JsonPropertyName("applicationStatus")]
         public ApplicationStatus ApplicationStatus { get; set; }
-
+        [JsonPropertyName("label")]
         public string? Label { get; set; }
-
+        [JsonPropertyName("createdOn")]
         public DateTime CreatedOn { get; set; }
-
+        [JsonPropertyName("CreatedBy")]
         public Guid CreatedBy { get; set; }
 
     }
