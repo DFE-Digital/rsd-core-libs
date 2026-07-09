26251c1e196d4ad3c08f7709e3fd6ff3ac39265c
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2026-07-09 11:43:28 +0100

    Added Created item to the ApplicationStatus Enum

--- src/GovUK.Dfe.CoreLibs.Contracts/ExternalApplications/Enums/ApplicationStatus.cs
+++ src/GovUK.Dfe.CoreLibs.Contracts/ExternalApplications/Enums/ApplicationStatus.cs
@@ -4,9 +4,13 @@ namespace GovUK.Dfe.CoreLibs.Contracts.ExternalApplications.Enums
 {
     public enum ApplicationStatus
     {
+        [Description("Created")]
+        Created = -1,
         [Description("In progress")]
         InProgress = 0,
         [Description("Submitted")]
-        Submitted = 1
+        Submitted = 1,
+        [Description("Deleted")]
+        Deleted = 2
     }
 }
