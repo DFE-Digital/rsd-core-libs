46287b9fe4825f98db13312a1e6e884f6c5a4ee9
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2026-09-25 10:58:46 +0100

    Added json property name for VerifyTestAuthPasswordResponse

--- src/GovUK.Dfe.CoreLibs.Contracts/ExternalApplications/Models/Response/VerifyTestAuthPasswordResponse.cs
+++ src/GovUK.Dfe.CoreLibs.Contracts/ExternalApplications/Models/Response/VerifyTestAuthPasswordResponse.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace GovUK.Dfe.CoreLibs.Contracts.ExternalApplications.Models.Response;
 
 /// <summary>
 /// Outcome of verifying a Test Authentication one-time password.
 /// </summary>
 /// <param name="IsValid"><c>true</c> when the password matched and has now been consumed.</param>
-public sealed record VerifyTestAuthPasswordResponse(bool IsValid);
+public sealed record VerifyTestAuthPasswordResponse(
+    [property: JsonPropertyName("isValid")] bool IsValid);
